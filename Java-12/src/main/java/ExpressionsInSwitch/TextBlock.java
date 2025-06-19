6707f8d76ec722abf9b903b53004978731b95961
ShrutiSugandhi <shrutisugandhi00@gmail.com>
2025-06-19 22:43:30 +0530

    Added Java-13 features

---
+++ Java-12/src/main/java/ExpressionsInSwitch/TextBlock.java
@@ -0,0 +1,13 @@
+package ExpressionsInSwitch;
+
+public class TextBlock {
+    public static void main(String[] args) {
+        String json= """
+                {
+                "name":"Shruti",
+                "company":"Epam systems"
+                }
+                """;
+        System.out.println(json);
+    }
+}
---
+++ Java-13/README.md
@@ -0,0 +1,7 @@
+**Java 13 Features**
+
+1. Switch Expressions
+   Yield:return a value from a block inside a switch expression,usefull for multiline switch.
+
+2.Text Blocks
+   writing multi-line strings like HTML, JSON, or SQL
